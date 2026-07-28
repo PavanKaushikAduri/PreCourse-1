2c2eb2509c77db9d123132a5815d64f5ce2fe054
Aduri Pavan Kaushik <apavankaushik@gmail.com>
2026-07-27 18:55:17 -0700

    Fix Exercise_2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,3 +1,13 @@
+# Time Complexity :
+#       push: O(1) - adding a new node at the head of the linked list.
+#       pop: O(1) - removing the node at the head of the linked list.
+# Space Complexity : O(N) - N is the number of elements in the stack, each element is stored as a node in the linked list.
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding : Kind of confused when trying to implement push. so basically when we try to add a new node, I think thinking vertically helps. so new code added should point to previous node which head points to and the head should point to this new node next. so we create a new node and then we set new_node.next to self.head and then we set self.head to new_node.
+
+
+# Your code here along with comments explaining your approach
+
 
 class Node:
     def __init__(self, data):
@@ -6,11 +16,24 @@ class Node:
  
 class Stack:
     def __init__(self):
-        
+        self.head = None
+
+
+    #Thinking vertically helps. lets assume we got a first push operation, so we create a new node with the given data and then we point new_node.next to self.head which is intially None and then we updat this head to point to this new node.
     def push(self, data):
-        
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+
+    # we already have self.head pointing to latest element, so we remove and return that element by getting value using self.head.data and then we move self.head to self.head.next so that head now points to the next element in the stack and we return data.
     def pop(self):
-        
+        if self.head is None:
+            return None
+        data = self.head.data
+        self.head = self.head.next
+        return data
+
 a_stack = Stack()
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
