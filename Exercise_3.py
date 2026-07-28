7eb03a300c75fc5dc09604d4c1d77ca5972922b7
Aduri Pavan Kaushik <apavankaushik@gmail.com>
2026-07-27 19:02:29 -0700

    Question3 Done

--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,8 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +19,15 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        new_node = ListNode(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
         
     def find(self, key):
         """
@@ -24,9 +35,27 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        current = self.head
+        while current:
+            if current.data == key:
+                return current
+            current = current.next
+        return None
+
+    #Used two pointers approach. We have prev=None and current=self.head. we check if current.data is key if it is key then we already have prev so we just do prev.next to current.next so this way we are removing the current element. if its not matching with key we just move our prev and current pointers.
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        current = self.head
+        prev = None
+        while current:
+            if current.data == key:
+                if prev:
+                    prev.next = current.next
+                else:
+                    self.head = current.next
+                return
+            prev = current
+            current = current.next
